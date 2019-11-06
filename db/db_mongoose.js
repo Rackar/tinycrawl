acbe854ecfbb09d567f1a0ba86998d5936168746
Your Name <27681135@qq.com>
2019-11-06 13:38:59 +0800

    ip

--- db/db_mongoose.js
+++ db/db_mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 // var testDB = 'mongodb://10.163.6.211:27017/rackar'
-var testDB = "mongodb://localhost:27017/rackar";
-// var testDB = "mongodb://172.17.0.5:27017/rackar";
+// var testDB = "mongodb://localhost:27017/rackar";
+var testDB = "mongodb://172.17.0.5:27017/rackar";
 mongoose.connect(
   testDB,
   { useNewUrlParser: true, useUnifiedTopology: true },
